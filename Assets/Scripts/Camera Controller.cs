4b8e7bd80dfd0627fd847848f849a34e62e0c678
mcst2014 <mtang44@ucsc.edu>
2026-04-20 22:00:30 -0500

    Player Camera Controls added

--- Assets/Scripts/Camera Controller.cs
+++ Assets/Scripts/Camera Controller.cs
@@ -2,15 +2,23 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    public Transform player;
+    public float mouseSensitivity = 3f;
+    float cameraVerticalRotation = 0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
+    // Update is called once per frame
     void Start()
     {
-        
+        Cursor.visible = false;
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        float inputX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        float inputY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+        cameraVerticalRotation -= inputY;
+        cameraVerticalRotation = Mathf.Clamp(cameraVerticalRotation, -90f,90f);
+        transform.localEulerAngles = Vector3.right * cameraVerticalRotation;
     }
 }
