26d03978995a4459768e5210158a58d8a553064a
Michael Stager <61891711+MichaelStager@users.noreply.github.com>
2026-04-23 00:35:29 -0700

    Game manager working, im spawning waves.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,10 +1,17 @@
+using NUnit.Framework;
 using UnityEngine;
-
+using System.Collections.Generic;
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
     bool isRoundActive;
-    public GameObject target;  //This might change from a gameObject to a NPC class object if we make one.
+    public NPCData target;
+    public List<NPCData> npcs;//This might change from a gameObject to a NPC class object if we make one.
+    public Sprite[] levelFacePool;
+    [SerializeField] GameObject npcPreFab;
+    //The parent for the new gameobjects to spawn into
+    [SerializeField] GameObject npcHolder;
+    int npcTotal = 10; 
     //float timer;
    // float maxTime = 120;
     private void Awake()
@@ -25,12 +32,20 @@ public class GameManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        StartWave();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void StartWave()
+    {
+        for(int i = 0; i < npcTotal; i++)
+        {
+            npcs.Add(Instantiate(npcPreFab, new Vector3(0, 0, 0), Quaternion.identity).GetComponent<NPCData>());
+        }
     }
 }
--- Assets/Scripts/NPCData.cs
+++ Assets/Scripts/NPCData.cs
@@ -3,14 +3,28 @@ using UnityEngine;
 public class NPCData : MonoBehaviour
 {
     bool isTarget = false;
+    //The face sprite of the npc
+    [SerializeField] SpriteRenderer npcFaceTarget;
+
+    private void Awake()
+    {
+        SetRandomFace();
+    }
     public void makeTarget()
     {
         isTarget = true;
-        GameManager.Instance.target = this.gameObject;
+        GameManager.Instance.target = this;
     }
 
     public bool getIsTarget()
     {
         return isTarget;
     }
+
+    public void SetRandomFace()
+    {
+       npcFaceTarget.sprite = GameManager.Instance.levelFacePool[Random.Range(0, GameManager.Instance.levelFacePool.Length)];
+    }
+
+    
 }
