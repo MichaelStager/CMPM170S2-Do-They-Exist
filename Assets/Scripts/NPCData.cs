2d0610444ebdf0c7c43988fa834a1dafbd1f7183
Michael Stager <61891711+MichaelStager@users.noreply.github.com>
2026-04-28 16:11:08 -0700

    COMPLETE INTERACTION OVERHAUL
    Completely over hauled the interaction system

--- Assets/Scripts/NPCData.cs
+++ Assets/Scripts/NPCData.cs
@@ -1,17 +1,30 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class NPCData : MonoBehaviour
 {
     public bool isTarget = false;
     //The face sprite of the npc
     public SpriteRenderer npcFaceTarget;
+    [SerializeField] GameObject yesNoDisplay;
+    InputController inputController;
+    
+    bool isBeingQuestioned;
 
     private void Awake()
     {
+        inputController = FindAnyObjectByType<InputController>();
+        isBeingQuestioned = false;
+        yesNoDisplay.SetActive(false);
         if (!isTarget)
         {
             SetRandomFace();
         }
+    }
+
+    private void Update()
+    {
+        
     }
 
     //To set the NPC you want to be the target. Will talk with gamemanger to get the target sprite and markdown who is the target.
@@ -33,5 +46,48 @@ public class NPCData : MonoBehaviour
        npcFaceTarget.sprite = GameManager.Instance.levelFacePool[Random.Range(0, GameManager.Instance.levelFacePool.Count)];
     }
 
-    
+
+    public void ClickedOn()
+    {
+        if (!isBeingQuestioned)
+        {
+            //This get a referance to the main camera of the scene to 
+            Camera.main.GetComponent<CameraController>().LookAtTarget(this);
+            isBeingQuestioned = true;
+            //display the yesNoDispaly
+            yesNoDisplay.SetActive(true);
+            //Lock the enemy
+            gameObject.GetComponent<NavMeshAgent>().enabled = false;
+            inputController.enabled = false;
+        }
+        else
+        {
+            HandleQuestioning();
+        }
+    }
+
+    void HandleQuestioning()
+    {
+        if (Input.GetMouseButtonDown(0) && isBeingQuestioned)
+        {
+            if(isTarget)
+            {
+                GameManager.Instance.StartWinSequence();
+                yesNoDisplay.SetActive(false);
+            }
+            else
+            {
+             GameManager.Instance.StartLoseSequence();
+            }
+        }
+        else if (Input.GetMouseButtonDown(1))
+        {
+            yesNoDisplay.SetActive(false);
+            isBeingQuestioned = false;
+            gameObject.GetComponent<NavMeshAgent>().enabled = true;
+            Camera.main.GetComponent<CameraController>().AllowControl();
+            inputController.enabled = true;
+
+        }
+    }
 }
