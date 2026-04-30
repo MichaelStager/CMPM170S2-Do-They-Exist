3b360c39d157e8c7b07b03a2ce2f5eeada7f9ff6
Robert Stager <queenbee.stager@gmail.com>
2026-04-30 11:11:45 -0700

    Made a new timer UI and shader for it

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,45 +1,16 @@
-using System.Drawing;
-using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
-    
-    [SerializeField] TextMeshProUGUI timerText;
-  //  [SerializeField] string preTimerText = "TIME LEFT TILL MELTDOWN:";
-    [SerializeField] Image timerImage;
-    [SerializeField] Material staticShaderMaterial;
+    [SerializeField] private Image timerImage;
 
-    [SerializeField] UnityEngine.Color startColor;
-    [SerializeField] UnityEngine.Color endColor;
-    [SerializeField] float staticStartThreadhold = 0.35f;
-
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        UpdateTimerFill();
-    }
-
-    void UpdateTimerFill()
-    {
-        // Starts at 1 then fractionally approaches 0
-        float timeProgress = Mathf.Clamp01(GameManager.Instance.currentLevelTime / GameManager.Instance.MAXLEVELTIME);
+        float timeProgress = Mathf.Clamp01(
+            GameManager.Instance.currentLevelTime / GameManager.Instance.MAXLEVELTIME
+        );
 
         timerImage.fillAmount = timeProgress;
-        if(timeProgress <= staticStartThreadhold) { staticShaderMaterial.SetFloat("_StaticStrength", 2 - timeProgress*2); }
-        else { staticShaderMaterial.SetFloat("_StaticStrength", 0); }
-
-        // Extract and declare HSV interpolation colors
-        UnityEngine.Color.RGBToHSV(startColor, out float startHue, out float startSaturation, out float startLight);
-        UnityEngine.Color.RGBToHSV(endColor, out float endHue, out float endSaturation, out float endLight);
-        // Lerps color from Green > Yellow > Red ( F00 > FF0 > 0F0 ) instead of Green > Brown > Red ( F00 > 770 > 0F0 )
-
-        // Put End HSV values at 0 and Start HSV values at 1. The full timer begins with the starting color then reaches the ending color as it empties
-        float hue = Mathf.LerpAngle(endHue, startHue, timeProgress);
-        float saturation = Mathf.Lerp(endSaturation, startSaturation, timeProgress);
-        float light = Mathf.Lerp(endLight, startLight, timeProgress);
-
-        timerImage.color = UnityEngine.Color.HSVToRGB(hue, saturation, light);
     }
 }
